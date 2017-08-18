1d1f078c97390a3e6a2cab79f6ad7401dea121eb
Zack Brown <zbrown@pillartechnology.com>
2017-08-18 09:01:22 -0400

    downloaded build steps from TC

--- .teamcity/ConwayRuby/Project.kt
+++ .teamcity/ConwayRuby/Project.kt
@@ -4,6 +4,9 @@ import ConwayRuby.buildTypes.*
 import ConwayRuby.vcsRoots.*
 import jetbrains.buildServer.configs.kotlin.v10.*
 import jetbrains.buildServer.configs.kotlin.v10.Project
+import jetbrains.buildServer.configs.kotlin.v10.projectFeatures.VersionedSettings
+import jetbrains.buildServer.configs.kotlin.v10.projectFeatures.VersionedSettings.*
+import jetbrains.buildServer.configs.kotlin.v10.projectFeatures.versionedSettings
 
 object Project : Project({
     uuid = "c6094632-b3c0-42ee-a078-ba2929af265c"
@@ -16,4 +19,11 @@ object Project : Project({
     buildType(ConwayRuby_BuildTest)
 
     template(ConwayRuby_Ruby)
+
+    features {
+        versionedSettings {
+            id = "PROJECT_EXT_4"
+            mode = VersionedSettings.Mode.DISABLED
+        }
+    }
 })
--- .teamcity/ConwayRuby/buildTypes/ConwayRuby_BuildTest.kt
+++ .teamcity/ConwayRuby/buildTypes/ConwayRuby_BuildTest.kt
@@ -4,15 +4,28 @@ import jetbrains.buildServer.configs.kotlin.v10.*
 import jetbrains.buildServer.configs.kotlin.v10.buildSteps.ScriptBuildStep
 import jetbrains.buildServer.configs.kotlin.v10.buildSteps.ScriptBuildStep.*
 import jetbrains.buildServer.configs.kotlin.v10.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v10.triggers.VcsTrigger
+import jetbrains.buildServer.configs.kotlin.v10.triggers.VcsTrigger.*
+import jetbrains.buildServer.configs.kotlin.v10.triggers.vcs
 
 object ConwayRuby_BuildTest : BuildType({
-    uuid = "5342d256-92db-46f0-a031-bd702493ffc6"
-    extId = "MyProject_Build"
-    name = "Build"
+    uuid = "8c094c6e-2665-4fe1-80af-eb7c83b26523"
+    extId = "ConwayRuby_BuildTest"
+    name = "Build / Test"
+
+    vcs {
+        root(ConwayRuby.vcsRoots.ConwayRuby_HttpsGithubComZtbrownConwayRubyRefsHeadsMaster)
+
+    }
 
     steps {
         script {
-            bundle install && rspec spec
+            scriptContent = "bundle install && rspec spec"
+        }
+    }
+
+    triggers {
+        vcs {
         }
     }
 })
