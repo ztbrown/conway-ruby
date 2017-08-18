373e23b6acb08459d0269e6e94fc61df6edb30a3
Zack Brown <zbrown@pillartechnology.com>
2017-08-18 10:05:31 -0400

    tc

--- .teamcity/ConwayRuby/vcsRoots/ConwayRuby_HttpsGithubComZtbrownConwayRubyRefsHeadsMaster.kt
+++ .teamcity/ConwayRuby/vcsRoots/ConwayRuby_HttpsGithubComZtbrownConwayRubyRefsHeadsMaster.kt
@@ -4,11 +4,10 @@ import jetbrains.buildServer.configs.kotlin.v10.*
 import jetbrains.buildServer.configs.kotlin.v10.vcs.GitVcsRoot
 
 object ConwayRuby_HttpsGithubComZtbrownConwayRubyRefsHeadsMaster : GitVcsRoot({
-    uuid = "9573bccc-9918-4e8d-9b5d-954c18acb0c3"
+    uuid = "dd0912bf-a904-4af4-a1e1-0dd0c95b0211"
     extId = "ConwayRuby_HttpsGithubComZtbrownConwayRubyRefsHeadsMaster"
     name = "https://github.com/ztbrown/conway-ruby#refs/heads/master"
     url = "https://github.com/ztbrown/conway-ruby"
-    branchSpec = "+:refs/heads/*"
     authMethod = password {
         userName = "ztbrown"
         password = "zxxd6aae2b36e2a803c45347a95c1ede63b2597f6e70b055896ed0f70419eecdbb8f280252fa33bad23775d03cbe80d301b"
