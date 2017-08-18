ed24aa82ff0975c361983baa9257a4c03341bc9f
Zack Brown <zbrown@pillartechnology.com>
2017-08-18 07:06:56 -0400

    yolo2

---
+++ .teamcity/ConwayRuby/Project.kt
@@ -0,0 +1,19 @@
+package ConwayRuby
+
+import ConwayRuby.buildTypes.*
+import ConwayRuby.vcsRoots.*
+import jetbrains.buildServer.configs.kotlin.v10.*
+import jetbrains.buildServer.configs.kotlin.v10.Project
+
+object Project : Project({
+    uuid = "c6094632-b3c0-42ee-a078-ba2929af265c"
+    extId = "ConwayRuby"
+    parentId = "_Root"
+    name = "Conway Ruby"
+
+    vcsRoot(ConwayRuby_HttpsGithubComZtbrownConwayRubyRefsHeadsMaster)
+
+    buildType(ConwayRuby_BuildTest)
+
+    template(ConwayRuby_Ruby)
+})
